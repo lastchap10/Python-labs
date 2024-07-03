277b2c79e7379560069fa49a7e84fcc1f421feb9
lastchap10 <laschap5@gmail.com>
2024-07-03 17:25:09 +0100

    update

--- test 2.py
+++ test 2.py
@@ -1,10 +1,1 @@
-secret_number = 7  # The secret number picked by the magician
-
-while True:
-    user_input = int(input("Enter an integer number: "))
-    if user_input == secret_number:
-        print(f"{user_input}")
-        print("Well done, muggle! You are free now.")
-        break
-    else:
-        print("Ha ha! You're stuck in my loop!")
+print(1584 % 4 )
