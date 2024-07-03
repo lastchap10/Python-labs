9d0cf9ef7a18437dcd59ad606996fa3279d38de1
lastchap10 <laschap5@gmail.com>
2024-07-03 01:05:33 +0100

    update

--- lab_2.6.11.py
+++ lab_2.6.11.py
@@ -1,18 +1,20 @@
-hour = int(input("Starting time (hours): ")) # user to input the starting hour, converts the input to an integer, and assigns it to the variable
-mins = int(input("Starting time (minutes): "))
-dura = int(input("Event duration (minutes): "))
+hour = int(input("Starting time (hours): ")) # user to input the starting time in hour, converts the input to an integer, 
+# and assigns it to the hour variable
+mins = int(input("Starting time (minutes): ")) # same as above for mins 
+dura = int(input("Event duration (minutes): ")) # same as above for duration
 
 # find a total of all minutes
-mins= mins+dura 
+mins= mins+dura # finding the total minutes by adding mins (start time) and the duration (in minutes) which gives you total minutes.
 
 # find a number of hours hidden in minutes and update the hour
 
-hour= hour+mins // 60 #Calculates how many whole hours are in the total minutes
+hour= hour+mins // 60 #calculating total hrs by adding hour variable (start time) and the whole hr
+# (total mins but converting duration mins into a whole hr using floor division)'''
 
 # correct minutes to fall in the (0..59) range
 
-mins = mins%60   #calculates the remainder when the total minutes are divided by 60
+mins = mins%60   # calculating total minute which is remainder of the minute, from the whole hr, using modulo operator.
 
 # correct hours to fall in the (0..23) range
-hour = hour % 24 #calculates the remainder when the total hours are divided by 24. This ensures the hour value falls within the range 0 to 23
+hour = hour % 24 #calculates the end time hour modulo by 24. This ensures the hour value falls within the range 0 to 23
 print(hour, ":", mins, sep='')
