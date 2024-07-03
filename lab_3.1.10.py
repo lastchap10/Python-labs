6d708a0c6ecf56202e3ad9ee894dd53cdf55cd65
lastchap10 <laschap5@gmail.com>
2024-07-03 01:28:48 +0100

    update

--- lab_3.1.10.py
+++ lab_3.1.10.py
@@ -1,12 +1,16 @@
 flower = input("type the name of the best flower: ")
+# assigning variables 
 best_flower = "Spathiphyllum"
 best_flower_lowercase = "spathiphyllum"
 best_flower1 = "pelargonium"
-if flower == best_flower:
-    print("Yes - Spathiphyllum is the best plant ever")
-    if flower == best_flower_lowercase:
-        print("No, I want a big Spathiphyllum!")
-    elif flower == best_flower1:
+
+if flower == best_flower: # if function to set a condition if it is true?
+    print("Yes - Spathiphyllum is the best plant ever") #prints the text if the condition is true
+
+if flower == best_flower_lowercase:
+        print("No, I want a big Spathiphyllum!") # prints the text if the condition is true
+
+if flower == best_flower1:
         print("Spathiphyllum! Not pelargonium!")
 else: 
-    print("No, I want a big Spathiphyllum!")
+    print("No, I want a big Spathiphyllum!") # if the line 7 condition is false, prints the text 
