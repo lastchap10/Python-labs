7810069fa4a558cc38e21b72a345f47626add541
lastchap10 <laschap5@gmail.com>
2024-07-04 19:41:43 +0100

    update

--- lab_3.4.11_Beatles.py
+++ lab_3.4.11_Beatles.py
@@ -8,17 +8,29 @@ step 5: use the insert() method to add Ringo Starr to the beginning of the list.
 beatles_list = []
 
 #step 2
-a = ("John Lennon", "Paul McCartney", "George Harrison")
-beatles_list.append(a)
+# a = "John Lennon", "Paul McCartney", "George Harrison" didn't work 
 
-print( beatles_list, "beatles")
+beatles_list.append("John Lennon") # adding strings in the list 
+beatles_list.append("Paul McCartney")
+beatles_list.append("George Harrison")
+
+print(beatles_list)
 
 #step 3 
-input_members = ["Stu Sutcliffe", "Pete Best"]
-for i in input_members: 
-    beatles_list.append(i)
-    
+input_members = ["Stu Sutcliffe", "Pete Best"] # assigning members in a list 
+
+for i in input_members:  # using for loop to add members
+    beatles_list.append(i) # using append i to add members 
+
 print( beatles_list, "beatles")
+
+#step 4
+del beatles_list[3] # deleting members from the list using the index
+del beatles_list[3]
+
+
 print( beatles_list, "beatles")
+
+beatles_list.insert(3, "Ringo Starr") # inserting new member in the position 3 
 print( beatles_list, "beatles")
-print( beatles_list, "beatles")
+
