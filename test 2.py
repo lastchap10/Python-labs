f2cd639435b77e85d6e077f1470690c727db2b42
lastchap10 <laschap5@gmail.com>
2024-07-09 00:18:25 +0100

    update

--- test 2.py
+++ test 2.py
@@ -1,3 +1,8 @@
-del numbers[1]
-print(len(numbers))
-print(numbers)
+my_list = [17, 3, 11, 5, 1, 9, 7, 15, 13]
+largest = my_list[0]
+ 
+for i in my_list[1:]:
+    if i > largest:
+        largest = i
+ 
+print(largest)
